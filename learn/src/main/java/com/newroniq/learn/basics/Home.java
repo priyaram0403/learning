4f419181e49a8fcf629d4a87f9669725aec735c3
webuser <webuser@priyas-air>
2019-07-29 23:07:08 +0100

    Method overloading

--- learn/src/main/java/com/newroniq/learn/basics/Home.java
+++ learn/src/main/java/com/newroniq/learn/basics/Home.java
@@ -22,11 +22,16 @@ public class Home {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		printhome("Airy and bright bedroom", 2);
+		printhome("Airy and bright bedroom");
 		
 	
 	}
 
 	public static void printhome(String name, int rooms) {
 		System.out.println(name + " - " + rooms);
+	}
+	
+	public static void printhome(String name) {
+		System.out.println(name);
 	}
 }
