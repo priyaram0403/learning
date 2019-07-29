2217020a1d6e0cd708538818a87099929051b7ac
priyaram0403 <53451304+priyaram0403@users.noreply.github.com>
2019-07-29 22:58:15 +0100

    Learning Java

---
+++ learn/src/main/java/com/newroniq/learn/basics/Home.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.newroniq.learn.basics;
+
+/**
+ * @author webuser
+ *
+ */
+public class Home {
+
+	/**
+	 * 
+	 */
+	public Home() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printhome("Airy and bright bedroom", 2);
+		
+	
+	}
+
+	public static void printhome(String name, int rooms) {
+		System.out.println(name + " - " + rooms);
+	}
+}
